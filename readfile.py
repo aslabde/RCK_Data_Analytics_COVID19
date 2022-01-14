f7862d7d1efe3e8d792cbf0c89db3f44fe7cb610
Eduardo Balsa <aslabde@gmail.com>
2022-01-14 12:12:46 +0100

    Initial workfiles added

---
+++ readfile.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+datos=pd.read_csv('casos_tecnica_ccaa.csv')
+datos.head()
+datos.info()
+
+cols =['fecha','num_casos']
+datos_filtrados = datos[cols]
+datos_filtrados.info()
+
+datos_agrupados = datos_filtrados.groupby('fecha').agg('sum')
+datos_agrupados.info()
+print(datos_agrupados)
+plt.scatter(x=datos_agrupados.fecha, y=datos_agrupados.num_casos)
+plt.show
