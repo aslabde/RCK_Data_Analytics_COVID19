b5f8df6a2b31c04010400d8b564d22d45ff7a285
Eduardo Balsa <aslabde@gmail.com>
2022-01-15 01:09:59 +0100

    Create csv file

--- readfile.py
+++ readfile.py
@@ -12,5 +12,7 @@ datos_filtrados.info()
 datos_agrupados = datos_filtrados.groupby('fecha').agg('sum')
 datos_agrupados.info()
 print(datos_agrupados)
-plt.scatter(x=datos_agrupados.fecha, y=datos_agrupados.num_casos)
-plt.show
+datos_agrupados_df=pd.DataFrame(datos_agrupados)
+datos_agrupados_df.to_csv('datos_agrupados.csv')
+#plt.scatter(x=datos_agrupados.fecha, y=datos_agrupados.num_casos)
+#plt.show
